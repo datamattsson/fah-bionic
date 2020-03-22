932b37de6f95fbaef4b34759086a4d121b8f6159
Michael Mattsson <michael.mattsson@nimblestorage.com>
2020-03-21 19:21:06 -0700

    bitly limitations
    Signed-off-by: Michael Mattsson <michael.mattsson@nimblestorage.com>

--- curlme.sh
+++ curlme.sh
@@ -3,7 +3,7 @@
 echo "deb http://archive.ubuntu.com/ubuntu/ bionic universe" > /etc/apt/sources.list.d/universe.list
 apt-get update
 apt-get install -y ansible
-curl -L datamattsson.io/installer.yaml -O installer.yaml
+curl -L datamattsson.io/fah-bionic-installer -O installer.yaml
 ansible-playbook installer.yaml
 
 echo "Access the FAH client web UI from http://localhost:8080"
