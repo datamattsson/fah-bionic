28e10d35a872f43fff48df7667e5de0fd6f690cf
Michael Mattsson <michael.mattsson@nimblestorage.com>
2020-03-21 19:34:10 -0700

    wget can't cope with interactive shells
    Signed-off-by: Michael Mattsson <michael.mattsson@nimblestorage.com>

--- curlme.sh
+++ curlme.sh
@@ -4,6 +4,8 @@ echo "deb http://archive.ubuntu.com/ubuntu/ bionic universe" > /etc/apt/sources.
 apt-get update
 apt-get install -y ansible
 wget -q datamattsson.io/fah-bionic-installer -O installer.yaml
-ansible-playbook installer.yaml
 
-echo "Access the FAH client web UI from http://localhost:8080"
+echo "Run the following to start the interactive installer:"
+echo
+echo "    ansible-playbook installer.yaml"
+echo
